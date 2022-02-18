4038d7f653fc3bbc7e29865786c6f13bb75e0dbd
AJL99 <a.loney@rgu.ac.uk>
2022-02-18 23:37:26 +0000

    made the output string more pretty

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //default values for data, d suffix stands for default
 var dCurrency = "GBP";
 var dAmount = 1;
-var dVAT = "DE";
+var dVAT = "IT";
 
 const amountInput = document.getElementById("input-lbl");
 const outputText = document.getElementById ("output-lbl");
@@ -26,7 +26,7 @@ request.onload = function() {
   let convertedNumber = JSON.stringify(response.rates); //this turns the amount of money into a string, but that string is messy and has waste that I want to remove
   let currencyToChop = convertedNumber.length; //due to number having different length depending on the inputed number, you need to know the exact length of the string so that you can change the substring command to always remove only the last character
   let outputNumber = convertedNumber.substring(7, currencyToChop-1); //this removed the first 7 characters from the string as the first 7 characters will always be the same amount of junk, it then removes the curly bracket at the end
-  outputText.textContent = outputNumber; //finally the amount in euros is shown with non of the junk
+  outputText.textContent = `${dAmount}` + ` ${dCurrency} = €` + outputNumber; //finally the amount in euros is shown with non of the junk
   }
 }
 //shows the user the VAT rates of a selected country, and shows how much they can expect prices to differ from the european average
