939d4a901861d286186fd67486cb0f5bb8c1d741
AJL99 <a.loney@rgu.ac.uk>
2022-04-15 02:00:12 +0100

    added local storage

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 //-------------------------------------------------------------------------------------------------
 window.onload = init; //runs the code after the HTML is loaded
 function init(){
-
+console.log(localStorage.getItem("country"));
 
 
 const contentFill = document.getElementById("content-shell");
@@ -175,88 +175,141 @@ function openCountryPage(){
 // yes I hardcored this but it wouldn't work without it. forgive me
   const buttonAT = document.getElementById("btn-AT");
   buttonAT.addEventListener("click", function(){
-    document.getElementById("debugP").innerHTML=`<img src="https://countryflagsapi.com/png/AT">`;
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/AT">`;
+    localStorage.setItem("country", "AT");
   });
 
   const buttonBE = document.getElementById("btn-BE");
   buttonBE.addEventListener("click", function(){
-    document.getElementById("debugP").innerHTML=`<img src="https://countryflagsapi.com/png/BE">`;
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/BE">`;
+    localStorage.setItem("country", "BE");
   });
   const buttonBG = document.getElementById("btn-BG");
-  buttonBG.addEventListener("click",countryChoice);
+  buttonBG.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/BG">`;
+    localStorage.setItem("country", "BG");
+  });
     
   const buttonHR = document.getElementById("btn-HR");
-  buttonHR.addEventListener("click",countryChoice);
-    
+  buttonHR.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/HR">`;
+    localStorage.setItem("country", "HR");
+  });
   const buttonCY = document.getElementById("btn-CY");
-  buttonCY.addEventListener("click",countryChoice);
-    
+  buttonCY.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/CY">`;
+    localStorage.setItem("country", "CY");
+  });
   const buttonCZ = document.getElementById("btn-CZ");
-  buttonCZ.addEventListener("click",countryChoice);
-    
+  buttonCZ.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/CZ">`;
+    localStorage.setItem("country", "CZ");
+  });
   const buttonDK = document.getElementById("btn-DK");
-  buttonDK.addEventListener("click",countryChoice);
-    
+  buttonDK.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/DK">`;
+    localStorage.setItem("country", "DK");
+  });
   const buttonEE = document.getElementById("btn-EE");
-  buttonEE.addEventListener("click",countryChoice);
-    
+  buttonEE.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/EE">`;
+    localStorage.setItem("country", "EE");
+  });
   const buttonFI = document.getElementById("btn-FI");
-  buttonFI.addEventListener("click",countryChoice);
-    
+  buttonFI.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/FI">`;
+    localStorage.setItem("country", "FI");
+  });
   const buttonFR = document.getElementById("btn-FR");
-  buttonFR.addEventListener("click",countryChoice);
-    
+  buttonFR.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/FR">`;
+    localStorage.setItem("country", "FR");
+  });
   const buttonDE = document.getElementById("btn-DE");
-  buttonDE.addEventListener("click",countryChoice);
-    
+  buttonDE.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/DE">`;
+    localStorage.setItem("country", "DE");
+  });
   const buttonGR = document.getElementById("btn-GR");
-  buttonGR.addEventListener("click",countryChoice);
-    
+  buttonGR.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/GR">`;
+    localStorage.setItem("country", "GR");
+  });
   const buttonHU = document.getElementById("btn-HU");
-  buttonHU.addEventListener("click",countryChoice);
-    
+  buttonHU.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/HU">`;
+    localStorage.setItem("country", "HU");
+  });
   const buttonIE = document.getElementById("btn-IE");
-  buttonIE.addEventListener("click",countryChoice);
-    
+  buttonIE.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/IE">`;
+    localStorage.setItem("country", "IE");
+  });
   const buttonIT = document.getElementById("btn-IT");
-  buttonIT.addEventListener("click",countryChoice);
-    
+  buttonIT.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/IT">`;
+    localStorage.setItem("country", "IT");
+  });
   const buttonLV = document.getElementById("btn-LV");
-  buttonLV.addEventListener("click",countryChoice);
-    
+  buttonLV.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/LV">`;
+    localStorage.setItem("country", "LV");
+  });
   const buttonLT = document.getElementById("btn-LT");
-  buttonLT.addEventListener("click",countryChoice);
-    
+  buttonLT.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/LT">`;
+    localStorage.setItem("country", "LT");
+  });
   const buttonLU = document.getElementById("btn-LU");
-  buttonLU.addEventListener("click",countryChoice);
-    
+  buttonLU.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/LU">`;
+    localStorage.setItem("country", "LU");
+  });
   const buttonMT = document.getElementById("btn-MT");
-  buttonMT.addEventListener("click",countryChoice);
-    
+  buttonMT.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/MT">`;
+    localStorage.setItem("country", "MT");
+  });
   const buttonNL = document.getElementById("btn-NL");
-  buttonNL.addEventListener("click",countryChoice);
-    
+  buttonNL.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/NL">`;
+    localStorage.setItem("country", "NL");
+  });
   const buttonPL = document.getElementById("btn-PL");
-  buttonPL.addEventListener("click",countryChoice);
-    
+  buttonPL.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/PL">`;
+    localStorage.setItem("country", "PL");
+  });
   const buttonPT = document.getElementById("btn-PT");
-  buttonPT.addEventListener("click",countryChoice);
-    
+  buttonPT.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/PT">`;
+    localStorage.setItem("country", "PT");
+  });
   const buttonRO = document.getElementById("btn-RO");
-  buttonRO.addEventListener("click",countryChoice);
-    
+  buttonRO.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/RO">`;
+    localStorage.setItem("country", "RO");
+  });
   const buttonSK = document.getElementById("btn-SK");
-  buttonSK.addEventListener("click",countryChoice);
-    
+  buttonSK.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/SK">`;
+    localStorage.setItem("country", "SK");
+  });
   const buttonSI = document.getElementById("btn-SI");
-  buttonSI.addEventListener("click",countryChoice);
-    
+  buttonSI.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/SI">`;
+    localStorage.setItem("country", "SI");
+  });
   const buttonES = document.getElementById("btn-ES");
-  buttonES.addEventListener("click",countryChoice);
-    
+  buttonES.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/ES">`;
+    localStorage.setItem("country", "ES");
+  });
   const buttonSE = document.getElementById("btn-SE");
-  buttonSE.addEventListener("click",countryChoice);
-
+  buttonSE.addEventListener("click", function(){
+    document.getElementById("chosenCountry").innerHTML=`<img src="https://countryflagsapi.com/png/SE">`;
+    localStorage.setItem("country", "SE");
+  });
   function countryChoice(){
     
 }
